9ac61d9f9de5a8b65645a43cae42096bcd3e0b6b
beyzabzr <113631271+beyzabzr@users.noreply.github.com>
2023-12-08 18:22:29 +0300

    main

---
+++ Hayvanlar.cs
@@ -0,0 +1,48 @@
+using System;
+namespace inheritance
+{
+    public class Hayvanlar : Canlilar
+    {
+        protected void Adaptasyon()
+        {
+            Console.WriteLine("Hayvanlar adaptasyon kurabilir");
+        }
+
+        public override void UyaranlaraTepki()
+        {
+            base.UyaranlaraTepki();
+            Console.WriteLine("Hayvanlar temasa tepki verir");
+        }
+    }
+
+    public class Surungenler : Hayvanlar
+    {
+        public Surungenler()
+        {
+            base.Adaptasyon();
+            base.Beslenme();
+            base.Bosaltim();
+            base.Solunum();
+        }
+        public void SurunerekHareketEtmek()
+        {
+            Console.WriteLine("Surungenler surunerek hareket ederler");
+        }
+    }
+
+    public class Kuslar : Hayvanlar
+    {
+        public Kuslar()
+        {
+            base.Adaptasyon();
+            base.Beslenme();
+            base.Bosaltim();
+            base.Solunum();
+            base.UyaranlaraTepki();
+        }
+        public void Ucmak()
+        {
+            Console.WriteLine("Kuslar ucar");
+        }
+    }
+}
